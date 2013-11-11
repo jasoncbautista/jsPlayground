b691b2cafa48fcfdaff8985a688ef70ae2564a4d
Jason <jason.bautista@sqor.com>
2013-11-11 15:39:44 -0800

    Formatted json correctly

--- ajax.js
+++ ajax.js
@@ -1,9 +1,11 @@
 function handler() {
     console.log("handled");
     if (client.readyState !== 4) {
-        console.log(this.responseXML);
-        console.log(this.responseJSON);
-        console.log(client.responseText);
+        if (client.responseText) {
+            console.log(client.responseText);
+            var _json = JSON.parse(client.responseText);
+            console.log(_json);
+        }
     }
 }
 
@@ -11,4 +13,4 @@ var client = new XMLHttpRequest();
 client.onreadystatechange = handler;
 client.open("GET", "sample.json");
 client.responseType = "json";
-client.send();
+client.send(null);
